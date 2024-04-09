185fd5c3e35c9e989de69f08f39a11fd91cf5f16
Павел Данилевич <pasha-danilevich@outlook.com>
2024-04-09 23:04:23 +0300

    auth settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 
 
@@ -20,7 +21,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'apps.api',
-    'apps.user'
+    'apps.user',
+    
+    'rest_framework',
+    'rest_framework.authtoken',
+    
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +116,71 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# AUTH
+
+# AUTH_USER_MODEL = 'apps.user.User'
+# AUTHENTICATION_BACKENDS = ('apps.user.backends.AuthBackend',)
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ]
+}
+SIMPLE_JWT = {
+    # кол-во дней для того
+    # чтобы зайти без пароля
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=7),
+    # после этого времени пароль обязателен
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    # при запросе на обновление токена, ЕСЛИ False, будет 
+    # возвращен только новый access-токен 
+    "ROTATE_REFRESH_TOKENS": True,
+    # будет ли refresh-токен добавлен в черный список после его обновления
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+
+    'AUTH_HEADER_TYPES': ('JWT', 'Beare'),
+
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+}
+
+
+
+DJOSER = {
+    # PASSWORD_RESET
+    'USER_CREATE_PASSWORD_RETYPE': True,
+    'SET_PASSWORD_RETYPE': True,
+    'PASSWORD_RESET_CONFIRM_RETYPE': True,
+    
+    
+    # вернет неверный ответ на запрос HTTP 400, если адрес электронной почты, 
+    # указанный для запроса на сброс пароля, не существует в базе данных
+    'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True,
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    
+    # EMAIL
+    'SEND_ACTIVATION_EMAIL': True,
+    'SEND_CONFIRMATION_EMAIL': True,
+    'ACTIVATION_URL': '#/activate/{uid}/{token}', # так будет выглядить url в писме на почту
+    
+    'TOKEN_MODEL': None, # we use only JWT (and migrate --fake)
+}
+
+# EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'django-backend@yandex.ru'
+EMAIL_HOST_PASSWORD = 'yumetfpebogtnbpk'
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
