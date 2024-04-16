f12e85a093f3220653986c7ae9ca4676e9203cac
Павел Данилевич <pasha-danilevich@outlook.com>
2024-04-16 22:42:31 +0300

    refactoring
    Переместил логику сборки профиля user из представления в ProfileSerializer (бывший UserSerializer)

--- src/apps/api/v1/user/serializers.py
+++ src/apps/api/v1/user/serializers.py
@@ -1,8 +1,35 @@
 from rest_framework import serializers
-from apps.user.models import User
 
-class UserSerializer(serializers.ModelSerializer):
+from apps.user.models import User, UserBookRelation
+from apps.word.models import UserWordRelation, Word
+
+
+def _get_quantity(cls, field, value):
+    return cls.objects.filter(**{field: value}).count()
+
+def _get_list_words(queryset):
+    return [{
+        "text": Word.objects.get(id=word.word_id).text, 
+        "translation": Word.objects.get(id=word.word_id).translation
+        }
+        for word in queryset
+    ]
+  
+class ProfileSerializer(serializers.ModelSerializer):
+    upload_book = serializers.SerializerMethodField()
+    studied_word = serializers.SerializerMethodField()
+    new_word = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['username']
-        
+        fields = ['username', 'upload_book', 'studied_word', 'new_word']
+
+    def get_upload_book(self, obj):
+        return _get_quantity(UserBookRelation, 'user_id', obj.id)
+
+    def get_studied_word(self, obj):
+        return _get_quantity(UserWordRelation, 'user_id', obj.id)
+
+    def get_new_word(self, obj):
+        queryset_new_word = UserWordRelation.objects.filter(user_id=obj.id)[:7]
+        return _get_list_words(queryset_new_word)
