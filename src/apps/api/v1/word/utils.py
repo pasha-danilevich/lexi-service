8dbe2a66f3597005a69e7a027835091ee42fb750
Павел Данилевич <pasha-danilevich@outlook.com>
2024-05-19 17:46:48 +0300

    Update utils.py

--- src/apps/api/v1/word/utils.py
+++ src/apps/api/v1/word/utils.py
@@ -17,18 +17,14 @@ def clean_string(text):
     return re.sub(r'[^a-zA-Zа-яА-Я]', '', text)
 
 
-def word_list_create(cls, word_set: dict, word: Word) -> list:
+def word_bulk_create(cls, word_set: dict, word: Word):
     if not word_set:
         return None
-
-    list = []
 
     for obj in word_set:
         obj.update({"word": word})
         obj = cls.objects.create(**obj)
-        list.append(obj)
 
-    return list
 
 
 def get_or_create_word(request_word):
@@ -43,9 +39,9 @@ def get_or_create_word(request_word):
 
         word = Word.objects.create(**word_set['word'])
 
-        word_list_create(Translation, word_set['translation'], word)
-        word_list_create(Synonym, word_set['synonym'], word)
-        word_list_create(Meaning, word_set['meaning'], word)
+        word_bulk_create(Translation, word_set['translation'], word)
+        word_bulk_create(Synonym, word_set['synonym'], word)
+        word_bulk_create(Meaning, word_set['meaning'], word)
 
         return word
 
