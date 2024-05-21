35922f5bb76c6ec6678cc6155439dd19bb485b82
Павел Данилевич <pasha-danilevich@outlook.com>
2024-05-21 22:17:04 +0300

    CORS

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'apps.word',
     
     'rest_framework',
+    'corsheaders',
     'djoser',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -209,3 +211,11 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 PAGE_SIZE = 2000
 PENALTY_SIZE = 100
+
+
+# CORS
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3080",
+    "http://localhost:5173",
+]
