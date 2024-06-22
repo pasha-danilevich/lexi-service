333e641325fd304b39979518dd16f694b0840287
Павел Данилевич <pasha-danilevich@outlook.com>
2024-06-22 22:31:07 +0300

    Update serializers.py

--- src/apps/api/v1/vocabulary/serializers.py
+++ src/apps/api/v1/vocabulary/serializers.py
@@ -21,34 +21,16 @@ class UserWordSerializer(serializers.ModelSerializer):
 class UserWordListSerializer(serializers.ModelSerializer):
     
     word = serializers.SerializerMethodField()
-    false_set = serializers.SerializerMethodField()
     
     class Meta:
         model = UserWord
-        fields = ["pk", "word", "false_set", "recognize_lvl", "reproduce_lvl"]
+        fields = ["pk", "word"]
         
-    def __init__(self, *args, **kwargs):
-        self.include_false_set = kwargs.pop('false_set', False)
-        self.number_of_false_set = kwargs.pop('number_of_false_set', False)
-        super(UserWordListSerializer, self).__init__(*args, **kwargs)
     
     def get_word(self, obj):
         word = obj.word
         fields = ('pk', 'text', 'part_of_speech', 'transcription')
         serializers = WordSerializer(word, fields=fields)
         data = serializers.data
-        data.update({'translation': word.translations.first().text})
         return data
     
-    def get_false_set(self, obj):
-        if self.include_false_set:
-            word = obj.word
-            
-            kwargs = {
-                "instance": word,
-                "part_of_speech": word.part_of_speech,
-                "number_of_false_set": self.number_of_false_set
-            }
-            
-            return get_false_set(**kwargs)
-        return None
