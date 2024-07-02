c1ce9a8f44227d7a74ed339880cf79d0ddd311a5
Павел Данилевич <pasha-danilevich@outlook.com>
2024-07-02 16:35:02 +0300

    googletrans / dictionaryapi

--- src/apps/api/v1/word/api.py
+++ src/apps/api/v1/word/api.py
@@ -44,3 +44,42 @@ class WordCreate(generics.GenericAPIView, mixins.CreateModelMixin):
         data = serializer.data
         
         return Response(data, status=status.HTTP_200_OK)
+
+from rest_framework.decorators import api_view
+from googletrans import Translator
+
+@api_view(['POST'])
+def googletrans(request):
+    text = request.data.get('text')
+    if not text:
+        return Response(data='Текст не передан', status=status.HTTP_400_BAD_REQUEST)
+
+    dest_language = request.data.get('dest_language', 'ru')
+
+    translator = Translator()
+
+    try:
+        translation = translator.translate(text, dest=dest_language)
+        print(translation)
+        translated_text = translation.text
+        return Response({'translated_text': translated_text}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'error': f"Translation error: {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+import requests
+
+@api_view(['POST'])
+def dictionaryapi(request):
+    request_word = clean_string(request.data.get('word', None))
+    
+    if not request_word:
+        return Response(data='Слово не передано', status=status.HTTP_400_BAD_REQUEST)
+    
+    url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{request_word}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return Response({'data': data}, status=status.HTTP_200_OK)
+    
+    return Response(data='Нет данных о слове', status=status.HTTP_404_NOT_FOUND)
