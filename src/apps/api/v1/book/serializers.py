0669a9895fed462110a5d99109171f75096de262
Павел Данилевич <pasha-danilevich@outlook.com>
2024-07-11 23:50:09 +0300

    slice_length

--- src/apps/api/v1/book/serializers.py
+++ src/apps/api/v1/book/serializers.py
@@ -45,7 +45,6 @@ class BookSerializer(serializers.ModelSerializer):
 
 
 class BookRetrieveSerializer(serializers.ModelSerializer):
-
     def __init__(self, *args, **kwargs):
         self.page = kwargs.pop('page')
         self.user = kwargs.pop('user', None)
@@ -54,6 +53,7 @@ class BookRetrieveSerializer(serializers.ModelSerializer):
     pages = serializers.SerializerMethodField()
     pages_slice = serializers.SerializerMethodField()
     bookmark = serializers.SerializerMethodField()
+    slice_length = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
@@ -64,22 +64,28 @@ class BookRetrieveSerializer(serializers.ModelSerializer):
             'author_upload',
             'page_count',
             'pages_slice',
+            'slice_length',
             'slug',
             'pages',
             'bookmark'
         ]
+
+    def get_slice_length(self, obj):
+        size_page_slice = 50
+        return size_page_slice
+
     def get_start_end(self, obj: Book):
         page = self.page - 1
-        size_page_slice = 50
+        size_page_slice = self.get_slice_length(obj)
 
         start = (page // size_page_slice) * size_page_slice
         end = (page // size_page_slice) * size_page_slice + size_page_slice
-        
+
         if end > obj.page_count:
             end = obj.page_count
-        
+
         return (start, end)
-    
+
     def get_pages_slice(self, obj: Book):
         start, end = self.get_start_end(obj)
         return [start + 1, end]
@@ -88,7 +94,7 @@ class BookRetrieveSerializer(serializers.ModelSerializer):
         start, end = self.get_start_end(obj)
         pages_set = obj.book[start:end]
         return pages_set
-    
+
     def get_bookmark(self, obj):
         return bookmark(self=self, obj=obj)
         
