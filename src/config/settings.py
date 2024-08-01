daca8ba485dae3db5f6217746e98d470eaeafa89
Павел Данилевич <pasha-danilevich@outlook.com>
2024-08-01 15:58:09 +0300

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -230,18 +230,18 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'level': 'DEBUG',
+#             'handlers': ['console'],
+#         },
+#     },
+# }
