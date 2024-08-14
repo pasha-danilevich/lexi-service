ede9ee5ec0bc24b17b767683fdb2a8dc4d047832
Павел Данилевич <pasha-danilevich@outlook.com>
2024-08-14 16:18:57 +0300

    bookmark -> bookmarks

--- src/apps/api/v1/search/urls.py
+++ src/apps/api/v1/search/urls.py
@@ -4,6 +4,6 @@ from .api import *
 urlpatterns = [
     path('books/', BookListSearch.as_view(), name='book-list-search'),
     path('books/my/', MyBookListSearch.as_view(), name='book-my-list-search'),
-    path('bookmark/', BookmarkListSearch.as_view(), name='bookmark-list-search'),
+    path('bookmarks/', BookmarkListSearch.as_view(), name='bookmark-list-search'),
     path('vocabulary/', VocabularyListSearch.as_view(), name='vocabulary-search'),
 ]
