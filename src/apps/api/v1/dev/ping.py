79a536828c45db3a4ec961435050520c43e5e452
Павел Данилевич <pasha-danilevich@outlook.com>
2024-08-20 00:55:47 +0300

    fix

--- src/apps/api/v1/dev/ping.py
+++ src/apps/api/v1/dev/ping.py
@@ -2,10 +2,24 @@ import requests
 import time
 from config.settings import SERVER_DOMAIN
 
+import requests
+import time
+import threading
+
+# Флаг для отслеживания состояния
+is_pinging = False
+
 def ping_server():
+    global is_pinging
     while True:
-        
-        response = requests.get(f'http://{SERVER_DOMAIN}/api/dev/ping/')
-        
-        time.sleep(20)  # 1 минута
-      
+        if not is_pinging:
+            is_pinging = True  # Устанавливаем флаг, чтобы предотвратить повторный запуск
+            try:
+                response = requests.get(f'http://{SERVER_DOMAIN}/api/dev/ping/')
+                print(f'Ping response status code: {response.status_code}')
+            except Exception as e:
+                print(f'Error pinging server: {e}')
+            finally:
+                is_pinging = False  # Сбрасываем флаг после завершения запроса
+
+        time.sleep(20)  # Ждем 20 секунд перед следующим запросом
