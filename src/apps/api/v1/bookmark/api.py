e7c5487c963eec1edca6294c7c6ce254982277e3
Павел Данилевич <pasha-danilevich@outlook.com>
2024-08-22 17:34:50 +0300

    bookmrks search

--- src/apps/api/v1/bookmark/api.py
+++ src/apps/api/v1/bookmark/api.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import QuerySet, Q
+
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,6 +15,17 @@ class BookmarkViewSet(viewsets.ModelViewSet):
     serializer_class = BookmarkListSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = BookmarkPageNumberPagination
+    
+    def filter_queryset(self, queryset: QuerySet):
+        # Получаем параметр поиска из запроса
+        search_query = self.request.query_params.get('search', None) # type: ignore
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(book__title__icontains=search_query) |
+                Q(book__author__icontains=search_query)
+            )
+        return super().filter_queryset(queryset)
 
     def get_queryset(self):
         return Bookmark.objects.filter(user_id=self.request.user.pk).order_by('-id')
